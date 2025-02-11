abc408541ef6a0c29d04265f95b25be384325693
Evaan <evaanhere@gmail.com>
2025-02-11 22:11:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let slideIndex = 0;
-const slides = document.querySelectorAll(".slide");
+window.onload = function() {
+    let slideIndex = 0;
+    const slides = document.querySelectorAll(".slide");
 
-function showSlides() {
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  // Hide all slides
+    function showSlides() {
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  // Hide all slides
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = "block";  // Show only the current slide
+        setTimeout(showSlides, 3000); // Change slide every 3 seconds
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";  // Show only the current slide
-    setTimeout(showSlides, 3000); // Change slide every 3 seconds
-}
 
-showSlides();
+    showSlides();
+};
